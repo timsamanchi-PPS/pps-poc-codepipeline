af8d5d8b362820344fd8369c76dad44b5fc7378a
tim samanchi <tim.samanchi@pps.edenred.com>
2022-04-21 22:36:04 +0100

    initial push
    codepipeline demo

--- codepipeline-role.tf
+++ codepipeline-role.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "pps-poc-codepipeline-Role" {
-  name = "pps-poc-codepipeline-Role"
+resource "aws_iam_role" "codepipeline-role" {
+  name = "codepipeline-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -8,13 +8,39 @@ resource "aws_iam_role" "pps-poc-codepipeline-Role" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-        Service = "codepipeline.amazonaws.com"
+          Service = "codepipeline.amazonaws.com"
         }
       },
     ]
   })
-  permissions_boundary = var.perms-boundary
+  permissions_boundary = "arn:aws:iam::714102873737:policy/pps.global.iamrp.ConanPermissionBoundaryPolicy"
+
   tags = {
-    Name = "pps-poc-codepipeline-Role"
+    Name = "codepipeline-role"
   }
+}
+data "aws_iam_policy_document" "policy-doc" {
+    statement {
+        sid = "statement1"
+        actions = ["codestar-connections:UseConnection"]
+        resources = ["*"]
+        effect = "Allow"
+    }
+    statement {
+        sid = "statement2"
+        actions = ["s3:*","codebuild:*","cloudwatch:*"]
+        resources = ["*"]
+        effect = "Allow"      
+    }
+}
+resource "aws_iam_policy" "codepipeline-policy" {
+    description = "codepipeline policy"
+    name = "codepipeline-policy"
+    path = "/"
+    policy = data.aws_iam_policy_document.policy-doc.json 
+}
+resource "aws_iam_role_policy_attachment" "test-attach" {
+
+    role = aws_iam_role.codepipeline-role.name
+    policy_arn = aws_iam_policy.codepipeline-policy.arn
 }
--- output.tf
+++
@@ -1,4 +0,0 @@
-output "codepipeline-artifacts-s3-bucket" {
-    description = "codepipeline build artifacts s3 bucket name"
-    value = var.codepipeline-artifacts
-}
